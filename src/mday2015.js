a4df5e79f3363bf5d55c88328feb1330202e0eff
Jesse Abramson <jr@jesseabramson.com>
2015-04-28 21:19:29 -0400

    old files out new files in

--- src/mday2015.js
+++ src/mday2015.js
@@ -11,7 +11,7 @@
   mdayApp.app_id = '81698a1c';
   mdayApp.app_key = '1e278291b92745ce12da510abe507606';
 
-  mdayApp.quotes = [];
+  mdayApp.quotes = []
   mdayApp.quotes.push(["<span>As far as I'm concerned,</span><span>there's no job more</span><span>important</span><span>on the planet than</span><span>being a</span><span>Mom.</span>"," - Mark Wahlberg"]);
   mdayApp.quotes.push(["<span>Mama,</span><span>she has</span><span>taught me well</span><span>Told me when I was young</span><span>Son, your life’s an</span><span>open book</span><span>Don’t close it</span><span>’fore it’s done</span>"," - Metallica"]);
   mdayApp.quotes.push(["<span>I owe much to</span><span>mother.</span><span>She had an expert's</span><span>understanding,</span><span>but also approached</span><span>art emotionally.</span>"," - David Rockefeller"]);
@@ -23,7 +23,7 @@
   mdayApp.quotes.push(["<span>You may have tangible</span><span>wealth untold.</span><span>Caskets of jewels</span><span>and coffers of gold.</span><span>Richer than I</span><span>you can never be —</span><span>I had a mother who</span><span>read to me.</span>"," - Strickland Gillilan"]);
   mdayApp.quotes.push(["<span>Mama</span><span>was my</span><span>greatest teacher, a</span><span>teacher of compassion,</span><span>love and fearlessness.</span><span>If love is sweet</span><span>as a flower,</span><span>then my mother is that</span><span>sweet flower</span><span>of love.</span>"," - Stevie Wonder"]);
 
-  mdayApp.data = [];
+  mdayApp.data = []
   mdayApp.data.push("&allowedIngredient[]=bacon&allowedIngredient[]=eggs&allowedIngredient[]=toast&excludedIngredient[]=avocado&flavor.sweet=0.3&maxResult=20&allowedCourse[]=course^course-Breakfast+and+Brunch&excludedCourse[]=course^course-Beverages");
   mdayApp.data.push("&allowedIngredient[]=eggs&allowedIngredient[]=onions&excludedIngredient[]=avocado&excludedIngredient[]=hot%20sauce&excludedIngredient[]=tomatoes&flavor.piquant=1&maxResult=20&allowedCourse[]=course^course-Breakfast+and+Brunch&excludedCourse[]=course^course-Beverages");
   mdayApp.data.push("&allowedIngredient[]=avocado&allowedIngredient[]=eggs&allowedIngredient[]=salsa&excludedIngredient[]=hummus&excludedIngredient[]=tortillas&excludedIngredient[]=wheat&maxResult=20&allowedCourse[]=course^course-Breakfast+and+Brunch&excludedCourse[]=course^course-Beverages");
@@ -35,13 +35,12 @@
   mdayApp.data.push("&allowedIngredient[]=eggs&allowedIngredient[]=salmon&flavor.sweet=0.3&maxResult=20&allowedCourse[]=course^course-Breakfast+and+Brunch&excludedCourse[]=course^course-Beverages");
   mdayApp.data.push("&allowedIngredient[]=strawberries&allowedIngredient[]=sugar&excludedIngredient[]=oats&flavor.meaty=0.6&flavor.sweet=0.9&maxResult=20&allowedCourse[]=course^course-Breakfast+and+Brunch&excludedCourse[]=course^course-Beverages");
   
-  mdayApp.viewed = [];
-
   function populateSearchResults( results ) { 
 
     mdayApp.results = results.matches;
     mdayApp.urls = results
     mdayApp.quote = mdayApp.quotes[$('#momType').prop("selectedIndex")];
+      console.log(mdayApp.results);
 
     $('#new').fadeIn().css("display","inline-block");
 
@@ -50,31 +49,15 @@
   
   function cycleResult() {
     randomResult = Math.floor(Math.random() * (mdayApp.results.length-1) - 1);
-    while ($.inArray( randomResult, mdayApp.viewed) !== -1) {
-      randomResult = Math.floor(Math.random() * (mdayApp.results.length-1) - 1);
-    }
     if (mdayApp.results[randomResult] === undefined) {
       randomResult = Math.floor(Math.random() * (mdayApp.results.length-1) - 1);
-
-      mdayApp.viewed.push(randomResult);
-      if (mdayApp.viewed.length === mdayApp.results.length) {
-        mdayApp.viewed = [];
-      }
-
-
+      console.log("bad index: " + randomResult);
       cycleResult();
     } else {
       $("#recipeImg").attr("src", mdayApp.results[randomResult].smallImageUrls[0].substring(0, mdayApp.results[randomResult].smallImageUrls[0].length-4));
       $("#recipeImg").attr("alt", mdayApp.results[randomResult].recipeName);
       $("#recipeTitle").text(mdayApp.results[randomResult].recipeName);
       $("#recipeUrl").attr("href", "http://www.yummly.com/recipe/" + mdayApp.results[randomResult].id);
-      console.log(mdayApp.results);
-      
-      mdayApp.viewed.push(randomResult);
-      if (mdayApp.viewed.length === mdayApp.results.length) {
-        mdayApp.viewed = [];
-      }
-
     }
   }
 
@@ -99,6 +82,7 @@
       $('.dropdown').css('left','0');
       $('.recipe').css('opacity','1');
       $('.theMoms div').css('display','none');
+      console.log($( "li" ).index( this ));
       $('.theMoms div:nth-child(' + ( $( "li" ).index( this ) + 1 ) + ')').fadeIn();
       // event.preventDefault();
       var yummlyAPI = "http://api.yummly.com/v1/api/recipes?_app_id=" + mdayApp.app_id + "&_app_key=" + mdayApp.app_key + mdayApp.data[$( "li" ).index( this )];
@@ -113,13 +97,58 @@
       $('#author').html(mdayApp.quotes[$( "li" ).index( this )][1]);
     });
 
-
-     $('.mom').click(function(){
-      var momIndex = ('0' + ($( "li" ).index( this ) + 1)).slice(-2)
+    $('.traditional').click(function(){
       $('#quote').removeClass();
-      $('#quote').addClass('quote' + momIndex );
-      $('.card').attr('href','pdf/MDAY2015-nesseco-card' + momIndex + '.pdf');
+      $('#quote').addClass('quote01');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card01.pdf');
     }); 
+    $('.rocker').click(function(){
+      $('#quote').removeClass();
+      $('#quote').addClass('quote02');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card02.pdf');
+    });
+    $('.artsy').click(function(){
+      $('#quote').removeClass();
+      $('#quote').addClass('quote03');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card03.pdf');
+    });
+    $('.hippie').click(function(){
+      $('#quote').removeClass();
+      $('#quote').addClass('quote04');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card04.pdf');
+    });
+    $('.sporty').click(function(){
+      $('#quote').removeClass();
+      $('#quote').addClass('quote05');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card05.pdf');
+    });
+    $('.disco').click(function(){
+      $('#quote').removeClass();
+      $('#quote').addClass('quote06');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card06.pdf');
+    });
+    $('.savvy').click(function(){
+      $('#quote').removeClass();
+      $('#quote').addClass('quote07');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card07.pdf');
+    });
+    $('.globetrotter').click(function(){
+      $('#quote').removeClass();
+      $('#quote').addClass('quote08');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card08.pdf');
+    });
+    $('.bookworm').click(function(){
+      $('#quote').removeClass();
+      $('#quote').addClass('quote09');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card09.pdf');
+    });
+    $('.sweetheart').click(function(){
+      $('#quote').removeClass();
+      $('#quote').addClass('quote10');
+      $('.card').attr('href','pdf/MDAY2015-nesseco-card10.pdf');
+    });
+
+
 
 
   });
